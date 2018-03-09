d7660b7c1366b2589924808f5db2b9248c9efe25
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2018-03-09 15:40:59 +0300

    Intial Commit:- sum of sub arrary

--- HackerEarthTraining/src/hackerearthtraining/closeToZero/CloseToZeroOption.java
+++ HackerEarthTraining/src/hackerearthtraining/closeToZero/CloseToZeroOption.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hackerearthtraining;
+package hackerearthtraining.closeToZero;
 
 import java.util.Arrays;
 
---
+++ HackerEarthTraining/src/hackerearthtraining/subArray/GFG.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerearthtraining.subArray;
+
+/**
+ *
+ * @author enovate
+ */
+// Java program to print largest contiguous
+// array sum
+import java.io.*;
+ 
+class GFG {
+ 
+    static int maxSubArraySum(int a[], int size)
+    {
+    int max_so_far = a[0];
+    int curr_max = a[0];
+ 
+    for (int i = 1; i < size; i++)
+    {
+           curr_max = Math.max(a[i], curr_max+a[i]);
+        max_so_far = Math.max(max_so_far, curr_max);
+    }
+    return max_so_far;
+    }
+ 
+    /* Driver program to test maxSubArraySum */
+    public static void main(String[] args)
+    {
+    int a[] = {4,8,5,2,4};
+    int n = a.length;   
+    int max_sum = maxSubArraySum(a, n);
+    System.out.println("Maximum contiguous sum is "
+                       + max_sum);
+    }
+}
+ 
+// This code is contributd by Prerna Saini
